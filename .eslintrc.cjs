84f90b7863f15a5364a050cdc8027f7d246f5a22
tarunkumarf22labs <119585731+tarunkumarf22labs@users.noreply.github.com>
2023-07-11 22:03:24 +0530

    @feat:configured eslint and prettier

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,40 +1,44 @@
 //@ts-nocheck
 module.exports = {
-    "env": {
-      "browser": true,
-      "es2021": true
-    },
-    "extends": [
-      "standard-with-typescript",
-      "plugin:react/recommended",
-      "prettier"
-    ],
-    "overrides": [
-      {
-        "env": {
-          "node": true
-        },
-        "files": [
-          ".eslintrc.{js,cjs}"
-        ],
-        "parserOptions": {
-          "sourceType": "script"
-        }
-      }
-    ],
-    "parser": "@typescript-eslint/parser",
-    "parserOptions": {
-      "ecmaVersion": "latest",
-      "sourceType": "module",
-      "project": "./tsconfig.json" // Add this line.
-    },
-    "plugins": [
-      "react",
-      "prettier"
-    ],
-    "rules": {
-        "react/react-in-jsx-scope": "off" ,// Add this line to disable the rule
-        "prettier/prettier": "error"
+  "env": {
+    "browser": true,
+    "es2021": true
+  },
+  "extends": [
+    "standard-with-typescript",
+    "plugin:react/recommended",
+    "plugin:prettier/recommended"
+  ],
+  "settings": { // Add this settings section
+    "react": {
+      "version": "detect"
     }
+  },
+  "overrides": [
+    {
+      "env": {
+        "node": true
+      },
+      "files": [
+        ".eslintrc.{js,cjs}"
+      ],
+      "parserOptions": {
+        "sourceType": "script"
+      }
+    }
+  ],
+  "parser": "@typescript-eslint/parser",
+  "parserOptions": {
+    "ecmaVersion": "latest",
+    "sourceType": "module",
+    "project": "./tsconfig.json"
+  },
+  "plugins": [
+    "react",
+    "prettier"
+  ],
+  "rules": {
+    "react/react-in-jsx-scope": "off",
+    "prettier/prettier": "error"
   }
-  
+}
